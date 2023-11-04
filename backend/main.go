f641869c362e99c1e436b180a4e60844b25c20a0
Arsalan <arsalan@kloudone.com>
2023-11-05 04:19:55 +0530

    add pkg/ws

--- backend/main.go
+++ backend/main.go
@@ -5,36 +5,8 @@ import (
 	"log"
 	"net/http"
 
-	"github.com/gorilla/websocket"
+	"pkg/websocket/"
 )
-
-// Upgrader requuires read and write buffer size
-var upgrader = websocket.Upgrader{
-	ReadBufferSize:  1024,
-	WriteBufferSize: 1024,
-	//need to find the origin of our con
-	//will allow to make req from react
-	//to here
-	CheckOrigin: func(r *http.Request) bool { return true },
-}
-
-// define reader to allow listen for new msgs to our ws ep
-func reader(conn *websocket.Conn) {
-	for {
-		//read msgs
-		messageType, p, err := conn.ReadMessage()
-		if err != nil {
-			log.Println(err)
-			return
-		}
-		fmt.Println(string(p))
-
-		if err := conn.WriteMessage(messageType, p); err != nil {
-			log.Println(err)
-			return
-		}
-	}
-}
 
 // define our ws ep
 func serveWs(w http.ResponseWriter, r *http.Request) {
@@ -45,12 +17,11 @@ func serveWs(w http.ResponseWriter, r *http.Request) {
 		log.Println(err)
 	}
 	//listen indefinetely
-	reader(ws)
+	go websocket.Writer(ws)
+	websocket.Reader(ws)
 }
 func setupRoutes() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Println(w, "simple server")
-	})
+	http.HandleFunc("/ws", serveWs)
 }
 
 func main() {
--- backend/pkg/websocket/websocket.go
+++ backend/pkg/websocket/websocket.go
@@ -1,6 +1,8 @@
 package websocket
 
 import (
+	"fmt"
+	"io"
 	"log"
 	"net/http"
 
@@ -20,4 +22,45 @@ func Upgrade(w http.ResponseWriter, r *http.Request) (*websocket.Conn, error) {
 		return ws, err
 	}
 	return ws, nil
+}
+
+func Reader(conn *websocket.Conn) {
+	for {
+		msgType, p, err := conn.ReadMessage()
+		if err != nil {
+			log.Println(err)
+			return
+		}
+
+		fmt.Println(string(p))
+
+		if err := conn.WriteMessage(msgType, p); err != nil {
+			log.Println(err)
+			return
+		}
+	}
+}
+
+func Writer(conn *websocket.Conn) {
+	for {
+		fmt.Println("Sending")
+		msgType, r, err := conn.NextReader()
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		w, err := conn.NextWriter(msgType)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		if _, err := io.Copy(w, r); err != nil {
+			fmt.Println(err)
+			return
+		}
+		if err := w.Close(); err != nil {
+			fmt.Println(err)
+			return
+		}
+	}
 }
